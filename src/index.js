8b12ff776d7dca76b055b7e6ffde73c7206fc21b
ayush.sha <ayush.sha@internal.media.net>
2024-12-22 18:24:43 +0530

    added google analytics code

--- src/index.js
+++ src/index.js
@@ -3,6 +3,13 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
+import ReactGA from "react-ga4";
+
+ReactGA.initialize("G-03SXXB93QS");
+ReactGA.send({
+  hitType: "pageview",
+  page: window.location.pathname
+});
 
 ReactDOM.render(
   
